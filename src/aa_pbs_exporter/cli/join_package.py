205f8a8808a9b80e290ad9a250f081fe20b761be
Chad Lowe <pfmsoft.dev@gmail.com>
2023-01-26 11:19:16 -0700

    Add split and join command skel

---
+++ src/aa_pbs_exporter/cli/join_package.py
@@ -0,0 +1,8 @@
+"""
+Cli interface to join packages into a package Group.
+
+"""
+
+import click
+
+from aa_pbs_exporter.models.bid_package_2022_10.bid_package import BidPackage
---
+++ src/aa_pbs_exporter/cli/split_package.py
@@ -0,0 +1,3 @@
+import click
+
+from aa_pbs_exporter.models.bid_package_2022_10.bid_package import BidPackage
