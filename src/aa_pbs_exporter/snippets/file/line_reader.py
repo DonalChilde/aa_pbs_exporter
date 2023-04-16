e0f5cfcd649137f5a5a4b07601500cc403dd17d7
Chad Lowe <pfmsoft.dev@gmail.com>
2023-04-16 09:17:44 -0700

    update line reader snippet

--- src/aa_pbs_exporter/snippets/file/line_reader.py
+++ src/aa_pbs_exporter/snippets/file/line_reader.py
@@ -1,3 +1,13 @@
+####################################################
+#                                                  #
+#          src/snippets/file/line_reader.py
+#                                                  #
+####################################################
+# Created by: Chad Lowe                            #
+# Created on: 2023-04-15T15:17:37-07:00            #
+# Last Modified: 2023-04-15T22:19:17.881469+00:00  #
+# Source: https://github.com/DonalChilde/snippets  #
+####################################################
 from pathlib import Path
 from typing import Iterator
 
