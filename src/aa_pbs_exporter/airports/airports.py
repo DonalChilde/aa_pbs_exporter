b53a0a4f057511b000a6f57f01a77a40f3dd9531
Chad Lowe <pfmsoft.dev@gmail.com>
2023-04-19 20:22:59 -0700

    add tz lookup

--- src/aa_pbs_exporter/airports/airports.py
+++ src/aa_pbs_exporter/airports/airports.py
@@ -27,3 +27,9 @@ def by_iata(iata: str, effective: datetime | None = None) -> Airport:
         effective=None,
     )
     return airport
+
+def tz_name_from_iata(iata:str)->str:
+    raw_airport = airports_iata.get(iata, None)
+    if raw_airport is None:
+        raise ValueError(f"iata code {iata} not found in database.")
+    return raw_airport["tz"]
