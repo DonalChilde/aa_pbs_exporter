428b014b344fdefb1d0d56e5ffe40ed56b888ac9
Chad Lowe <pfmsoft.dev@gmail.com>
2023-04-24 19:32:41 -0700

    make ParseResult a pydantic BaseModel

--- src/aa_pbs_exporter/pbs_2022_01/parse_result.py
+++ src/aa_pbs_exporter/pbs_2022_01/parse_result.py
@@ -1,9 +1,13 @@
-from dataclasses import dataclass
-
+# from dataclasses import dataclass
+from pydantic import BaseModel
 from aa_pbs_exporter.pbs_2022_01.models import raw
 
 
-@dataclass
-class ParseResult:
+# @dataclass
+# class ParseResult:
+#     current_state: str
+#     parsed_data: raw.ParsedIndexedString
+
+class ParseResult(BaseModel):
     current_state: str
     parsed_data: raw.ParsedIndexedString
