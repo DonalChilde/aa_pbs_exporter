6a9ad8e73c2aa8bdd1be0c1dda08bb04952c2b94
Chad Lowe <pfmsoft.dev@gmail.com>
2023-04-29 08:41:29 -0700

    log validation messages

--- src/aa_pbs_exporter/pbs_2022_01/validation_helper.py
+++ src/aa_pbs_exporter/pbs_2022_01/validation_helper.py
@@ -1,8 +1,14 @@
+import logging
+
 from aa_pbs_exporter.snippets.messages.message import Message
 from aa_pbs_exporter.snippets.messages.publisher import Publisher
 
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
 
 def send_validation_message(msg: Message, ctx: dict | None):
+    logger.warning(msg=f"{msg}")
     if ctx is None:
         return
     publisher: Publisher | None = ctx.get("validation_publisher", None)
