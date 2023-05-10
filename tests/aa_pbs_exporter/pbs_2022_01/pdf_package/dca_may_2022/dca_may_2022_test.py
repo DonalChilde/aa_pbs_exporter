99c9c7fafd551d4fb639eb768ff26a8029a4025a
Chad Lowe <pfmsoft.dev@gmail.com>
2023-05-10 07:15:24 -0700

    add pdf parsing tests

--- tests/aa_pbs_exporter/pbs_2022_01/pdf_package/dca_may_2022/dca_may_2022_test.py
+++ tests/aa_pbs_exporter/pbs_2022_01/pdf_package/dca_may_2022/dca_may_2022_test.py
@@ -2,9 +2,10 @@ from importlib import resources
 from logging import Logger
 from pathlib import Path
 
+import pytest
 from tests.aa_pbs_exporter.resources.helpers_2 import ResourceTestData
 
-from aa_pbs_exporter.pbs_2022_01.parse_pbs_pdf import parse_pbs_pdf
+from aa_pbs_exporter.pbs_2022_01.parse_pbs_pdf_file import parse_pbs_pdf_file
 
 SERIALIZE_ONLY = False
 PDF_TEST_DATA = ResourceTestData(
@@ -15,16 +16,17 @@ COMPACT_TEST_DATA = ResourceTestData("", "one_page_compact.json")
 EXPANDED_TEST_DATA = ResourceTestData("", "one_page_expanded.json")
 
 
+@pytest.mark.slow
 def test_pdf(test_app_data_dir: Path, logger: Logger):
     output_path = (
         test_app_data_dir
-        / "pdf"
+        / "pdf_package"
         / "dca_may_2022"
         / __name__.rsplit(".", maxsplit=1)[-1]
     )
     res_dir = resources.files(__package__)
     res_file = res_dir.joinpath(PDF_TEST_DATA.input_data)
-    # raw_package, compact_package, expanded_package = parse_pbs_pdf(
-    #     pdf_file=res_file,
-    #     output_dir=output_path,
-    # )
+    raw_package, compact_package, expanded_package = parse_pbs_pdf_file(
+        pdf_file=res_file,
+        output_dir=output_path,
+    )
