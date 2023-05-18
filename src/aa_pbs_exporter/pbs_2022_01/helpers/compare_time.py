e60245ed3c8f22c08862ba215c576d87b00f42c4
Chad Lowe <pfmsoft.dev@gmail.com>
2023-05-18 05:16:09 -0700

    fix spelling

--- src/aa_pbs_exporter/pbs_2022_01/helpers/compare_time.py
+++ src/aa_pbs_exporter/pbs_2022_01/helpers/compare_time.py
@@ -4,7 +4,7 @@ from datetime import time
 def compare_time(time_1: time, time_2: time, ignore_tz: bool = False) -> bool:
     # TODO make snippet
     if ignore_tz:
-        nieve_time_1 = time_1.replace(tzinfo=None)
-        nieve_time_2 = time_2.replace(tzinfo=None)
-        return nieve_time_1 == nieve_time_2
+        naive_time_1 = time_1.replace(tzinfo=None)
+        naive_time_2 = time_2.replace(tzinfo=None)
+        return naive_time_1 == naive_time_2
     return time_1 == time_2
