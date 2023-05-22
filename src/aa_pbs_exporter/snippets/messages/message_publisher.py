10d4bf7a06ec44d0af9a532338e5ef39764721a4
Chad Lowe <pfmsoft.dev@gmail.com>
2023-05-22 06:00:46 -0700

    update messenger snippet

--- src/aa_pbs_exporter/snippets/messages/message_publisher.py
+++ src/aa_pbs_exporter/snippets/messages/message_publisher.py
@@ -5,22 +5,22 @@
 ####################################################
 # Created by: Chad Lowe                            #
 # Created on: 2023-04-26T11:15:44-07:00            #
-# Last Modified: 2023-05-04T13:50:18.033276+00:00  #
+# Last Modified: 2023-05-22T12:50:58.352838+00:00  #
 # Source: https://github.com/DonalChilde/snippets  #
 ####################################################
 from typing import Sequence
 
-from aa_pbs_exporter.snippets.messages.messenger_protocol import (
+from snippets.messages.messenger_protocol import (
     MessageProtocol,
     MessagePublisherProtocol,
-    MessengerConsumerProtocol,
+    MessengeListenerProtocol,
 )
 
 
-class Publisher(MessagePublisherProtocol):
-    def __init__(self, consumers: Sequence[MessengerConsumerProtocol]) -> None:
+class MessagePublisher(MessagePublisherProtocol):
+    def __init__(self, consumers: Sequence[MessengeListenerProtocol]) -> None:
         self.consumers = list(consumers)
 
     def publish_message(self, msg: MessageProtocol):
         for consumer in self.consumers:
-            consumer.consume_message(msg=msg)
+            consumer.receive_message(msg=msg)
