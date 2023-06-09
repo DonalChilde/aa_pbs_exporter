31b585031921fbcede1df61d111f384454a26b6f
Chad Lowe <pfmsoft@gmail.com>
2023-06-09 11:08:02 -0700

    add default date

--- scripts/parse_all_the_things.sh
+++ scripts/parse_all_the_things.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Accepts Params
-#   YEAR - the year to parse.
+#   YEAR - the year to parse. Default 2023
 
 declare -A DATA_DIRS
 DATA_DIRS[2020]="11 12"
@@ -10,7 +10,7 @@ DATA_DIRS[2022]="01 02 03 04 05 06 07 08 09 10 11 12"
 DATA_DIRS[2023]="01 03 03 04 05 06"
 
 DATE_STRING="$(date -u "+%Y%m%dT%H%M%sZ")"
-PATH_YEAR=$1
+PATH_YEAR=${1:-"2023"}
 
 IFS=' '
 read -a MONTHS <<<"${DATA_DIRS[$PATH_YEAR]}"
