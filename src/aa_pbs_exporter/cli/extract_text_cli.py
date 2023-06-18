ac032cacb58ab5ab3bc22ea7c0dd5d320e206e20
Chad Lowe <pfmsoft@gmail.com>
2023-06-18 09:28:07 -0700

    Add multiprocessing support to pdf to text extract

--- src/aa_pbs_exporter/cli/extract_text_cli.py
+++ src/aa_pbs_exporter/cli/extract_text_cli.py
@@ -5,9 +5,14 @@ import click
 from aa_pbs_exporter.snippets.click.task_complete import task_complete
 from aa_pbs_exporter.snippets.pdf.extract_text_from_pdf_to_file import (
     extract_text_from_pdf_to_file,
+    ExtractJob,
+    do_extract_job,
+    do_extract_jobs,
 )
+from time import perf_counter_ns
 
 
+# TODO make snippet
 @click.command()
 @click.pass_context
 @click.argument("path_in", type=click.Path(exists=True, path_type=Path))
@@ -27,11 +32,20 @@ from aa_pbs_exporter.snippets.pdf.extract_text_from_pdf_to_file import (
     help="Specify a filename to use when exporting a single file. A .txt suffix will "
     "be added to the end of the filename.",
 )
+@click.option(
+    "--processes",
+    "-p",
+    type=int,
+    default=4,
+    show_default=True,
+    help="Specify the number of processors to use when extracting multiple files. Use -1 for max.",
+)
 def extract(
     ctx: click.Context,
     path_in: Path,
     path_out: Path,
     overwrite: bool,
+    processes: int,
     name: Path | None,
 ):
     """Extract the text from one or more pdf files to new text files.
@@ -48,48 +62,86 @@ def extract(
             ctx=ctx,
         )
     if path_in.is_file():
-        if name is None:
-            extract_text_keep_name(
-                file_in=path_in, dir_out=path_out, overwrite=overwrite
-            )
-            task_complete(ctx=ctx)
-            return 0
         if name == Path(""):
             raise click.UsageError(
                 f"Possible invalid file name {name}. Is it an empty string?",
                 ctx=ctx,
             )
-
-        extract_text_new_name(path_in, dir_out=path_out, name=name, overwrite=overwrite)
+        if name is None:
+            file_out = keep_name(file_in=path_in, dir_out=path_out)
+        else:
+            file_out = new_name(dir_out=path_out, name=name)
+        job = make_job(file_in=path_in, file_out=file_out, overwrite=overwrite)
+        click.echo(f"Extracting {path_in.name} to {file_out}")
+        do_extract_job(job)
         task_complete(ctx=ctx)
         return 0
 
     if path_in.is_dir():
         pdf_files = list(path_in.glob("*.pdf"))
         click.echo(f"Found {len(pdf_files)} files.")
+        click.echo(f"Extracting to {path_out}")
         if not pdf_files:
             raise click.UsageError(f"No pdf files found at {path_in}", ctx=ctx)
+        jobs: list[ExtractJob] = []
         for idx, pdf_file in enumerate(pdf_files):
-            click.echo(f"\nExtracting {idx+1} of {len(pdf_files)} files.")
-            extract_text_keep_name(
-                file_in=pdf_file, dir_out=path_out, overwrite=overwrite
+            file_out = keep_name(pdf_file, path_out)
+            job = make_job(
+                file_in=pdf_file,
+                file_out=file_out,
+                overwrite=overwrite,
+                job_id=f"{pdf_file.name} - {idx+1} of {len(pdf_files)}",
             )
+            jobs.append(job)
+        if processes == -1:
+            proc = None
+        else:
+            proc = processes
+        do_extract_jobs(jobs, processes=proc)
         task_complete(ctx=ctx)
         return 0
 
 
-def extract_text_new_name(
-    file_in: Path, dir_out: Path, name: Path, overwrite: bool = False
-):
+def make_job(
+    file_in: Path, file_out: Path, overwrite: bool = False, job_id: str = ""
+) -> ExtractJob:
+    job = ExtractJob(
+        file_in=file_in,
+        file_out=file_out,
+        overwrite=overwrite,
+        job_id=job_id,
+        start_callback=start_cb,
+        finish_callback=finish_cb,
+    )
+    return job
+
+
+def start_cb(job: ExtractJob):
+    job.start = perf_counter_ns()
+
+
+def finish_cb(job: ExtractJob):
+    job.end = perf_counter_ns()
+    if job.start is not None:
+        length = job.end - job.start
+        click.echo(f"Extracted {job.job_id} in {length/1000000000:9f} seconds.")
+        return
+    click.echo(f"Extracted {job.job_id}")
+
+
+def new_name(
+    dir_out: Path,
+    name: Path,
+) -> Path:
     file_out = dir_out / append_suffix(name, ".txt")
-    extract_text(file_in=file_in, file_out=file_out, overwrite=overwrite)
+    return file_out
 
 
-def extract_text_keep_name(file_in: Path, dir_out: Path, overwrite: bool = False):
+def keep_name(file_in: Path, dir_out: Path) -> Path:
     original_name = Path(file_in.name)
-    new_name = append_suffix(original_name, ".txt")
-    file_out = dir_out / new_name
-    extract_text(file_in=file_in, file_out=file_out, overwrite=overwrite)
+    name = append_suffix(original_name, ".txt")
+    file_out = dir_out / name
+    return file_out
 
 
 def extract_text(file_in: Path, file_out: Path, overwrite: bool = False):
