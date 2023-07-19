0636904ea6f3b409b5158cab21514965bdc68821
Chad Lowe <pfmsoft@gmail.com>
2023-07-19 10:23:15 -0700

    fix serialise method name

--- tests/aa_pbs_exporter/pbs_2022_01/validate/validate_compact_test.py
+++ tests/aa_pbs_exporter/pbs_2022_01/validate/validate_compact_test.py
@@ -22,7 +22,7 @@ def test_compact_validation(test_app_data_dir: Path, logger: Logger):
         debug_path = output_path / f"{raw_path.stem}-validate-debug.txt"
     with resources.as_file(compact_bid_package_res.file_resource()) as compact_path:
         compact_loader = SerializePydantic[compact.BidPackage]()
-        compact_bid_package = compact_loader.load_from_json_file(
+        compact_bid_package = compact_loader.load_json(
             compact.BidPackage, file_in=compact_path
         )
     validate_compact_bid_package(
