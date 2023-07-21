b8122a76c781b934ae7684571e80e78ed92fd0d5
Chad Lowe <pfmsoft@gmail.com>
2023-07-21 06:35:49 -0700

    change compact validation entry point

--- src/aa_pbs_exporter/pbs_2022_01/validate/__init__.py
+++ src/aa_pbs_exporter/pbs_2022_01/validate/__init__.py
@@ -1,6 +1,6 @@
-from aa_pbs_exporter.pbs_2022_01.validate.validate_compact_td import ValidateCompact
+from aa_pbs_exporter.pbs_2022_01.validate.compact import validate_compact_bid_package
 from aa_pbs_exporter.pbs_2022_01.validate.validate_expanded import ExpandedValidator
 
 # from aa_pbs_exporter.pbs_2022_01.validate.validate_raw import RawValidator
 
-__all__ = ["ValidateCompact", "ExpandedValidator"]
+__all__ = ["validate_compact_bid_package", "ExpandedValidator"]
