497ea8ca82707c6208b6b990ce25d0e3e7a5526f
Chad Lowe <pfmsoft@gmail.com>
2023-09-02 07:28:20 -0700

    comment old code prior to removal

--- src/aa_pbs_exporter/cli/parse_text.py
+++ src/aa_pbs_exporter/cli/parse_text.py
@@ -1,76 +1,78 @@
-from pathlib import Path
+# from pathlib import Path
 
-import click
+# import click
 
-from aa_pbs_exporter.pbs_2022_01.parse_pbs_txt_file import parse_pbs_txt_file
-from aa_pbs_exporter.snippets.click.task_complete import task_complete
-from aa_pbs_exporter.snippets.indexed_string.state_parser.parse_exception import (
-    ParseException,
-)
+# from aa_pbs_exporter.pbs_2022_01.api import parse_pbs_txt_file
+# from aa_pbs_exporter.snippets.click.task_complete import task_complete
+# from aa_pbs_exporter.snippets.indexed_string.state_parser.parse_exception import (
+#     ParseException,
+# )
 
-
-@click.command()
-@click.pass_context
-@click.argument("path_in", type=click.Path(exists=True, path_type=Path))
-@click.argument("path_out", type=Path)
-@click.option("--debug-path", "-d", type=Path, default=None)
-@click.option("--debug/--no-debug", default=True, show_default=True)
-@click.option("--compact/--no-compact", default=True, show_default=True)
-@click.option("--expand/--no-expand", default=True, show_default=True)
-def parse(
-    ctx: click.Context,
-    path_in: Path,
-    path_out: Path,
-    debug: bool,
-    debug_path: Path | None,
-    compact: bool,
-    expand: bool,
-) -> int:
-    """Parse a pairing package text file to json or yaml.
-
-    PATH_IN can be a single txt file, or a directory containing one or more
-    txt files. PATH_OUT must be a directory which will contain the output files. Missing
-    directories will be created as needed.
-    """
-    if path_out.exists() and not path_out.is_dir():
-        raise click.UsageError(
-            f"PATH_OUT {path_out} is not a directory.",
-            ctx=ctx,
-        )
-    if path_in.is_file():
-        # parse_pbs_txt_file(txt_file=path_in, output_dir=path_out)
-        parse_the_file(path_out=path_out, txt_file=path_in)
-        task_complete(ctx=ctx)
-        return 0
-
-    if path_in.is_dir():
-        txt_files = list(path_in.glob("*.txt"))
-        click.echo(f"Checking {path_in} for files.")
-        click.echo(f"Found {len(txt_files)} files.")
-        if not txt_files:
-            raise click.UsageError(f"No txt files found at {path_in}", ctx=ctx)
-        for idx, txt_file in enumerate(txt_files):
-            click.echo(f"\nParsing {idx+1} of {len(txt_files)} files.")
-            parse_the_file(path_out=path_out, txt_file=txt_file)
-            # click.echo(f"Parsing {txt_file.stem}")
-            # sub_dir = path_out / txt_file.stem
-            # try:
-            #     parse_pbs_txt_file(txt_file=txt_file, output_dir=sub_dir)
-            # except ParseException as error:
-            #     click.echo(f"There was an error parsing {txt_file}")
-            #     click.echo("Check the logs for more details.")
-            #     click.echo(error)
-        task_complete(ctx=ctx)
-        return 0
-    raise click.UsageError("There was an unexpected error.")
+# # TODO make this parse only, use translate for next steps.
 
 
-def parse_the_file(path_out: Path, txt_file: Path):
-    sub_dir = path_out / txt_file.name.split(".")[0]
-    click.echo(f"Parsing {txt_file} to {sub_dir}")
-    try:
-        parse_pbs_txt_file(txt_file=txt_file, output_dir=sub_dir, debug_dir=sub_dir)
-    except ParseException as error:
-        click.echo(f"There was an error parsing {txt_file}")
-        click.echo("Check the logs for more details.")
-        click.echo(error)
+# @click.command()
+# @click.pass_context
+# @click.argument("path_in", type=click.Path(exists=True, path_type=Path))
+# @click.argument("path_out", type=Path)
+# @click.option("--debug-path", "-d", type=Path, default=None)
+# @click.option("--debug/--no-debug", default=True, show_default=True)
+# @click.option("--compact/--no-compact", default=True, show_default=True)
+# @click.option("--expand/--no-expand", default=True, show_default=True)
+# def parse(
+#     ctx: click.Context,
+#     path_in: Path,
+#     path_out: Path,
+#     debug: bool,
+#     debug_path: Path | None,
+#     compact: bool,
+#     expand: bool,
+# ) -> int:
+#     """Parse a pairing package text file to json or yaml.
+
+#     PATH_IN can be a single txt file, or a directory containing one or more
+#     txt files. PATH_OUT must be a directory which will contain the output files. Missing
+#     directories will be created as needed.
+#     """
+#     if path_out.exists() and not path_out.is_dir():
+#         raise click.UsageError(
+#             f"PATH_OUT {path_out} is not a directory.",
+#             ctx=ctx,
+#         )
+#     if path_in.is_file():
+#         # parse_pbs_txt_file(txt_file=path_in, output_dir=path_out)
+#         parse_the_file(path_out=path_out, txt_file=path_in)
+#         task_complete(ctx=ctx)
+#         return 0
+
+#     if path_in.is_dir():
+#         txt_files = list(path_in.glob("*.txt"))
+#         click.echo(f"Checking {path_in} for files.")
+#         click.echo(f"Found {len(txt_files)} files.")
+#         if not txt_files:
+#             raise click.UsageError(f"No txt files found at {path_in}", ctx=ctx)
+#         for idx, txt_file in enumerate(txt_files):
+#             click.echo(f"\nParsing {idx+1} of {len(txt_files)} files.")
+#             parse_the_file(path_out=path_out, txt_file=txt_file)
+#             # click.echo(f"Parsing {txt_file.stem}")
+#             # sub_dir = path_out / txt_file.stem
+#             # try:
+#             #     parse_pbs_txt_file(txt_file=txt_file, output_dir=sub_dir)
+#             # except ParseException as error:
+#             #     click.echo(f"There was an error parsing {txt_file}")
+#             #     click.echo("Check the logs for more details.")
+#             #     click.echo(error)
+#         task_complete(ctx=ctx)
+#         return 0
+#     raise click.UsageError("There was an unexpected error.")
+
+
+# def parse_the_file(path_out: Path, txt_file: Path):
+#     sub_dir = path_out / txt_file.name.split(".")[0]
+#     click.echo(f"Parsing {txt_file} to {sub_dir}")
+#     try:
+#         parse_pbs_txt_file(txt_file=txt_file, output_dir=sub_dir, debug_dir=sub_dir)
+#     except ParseException as error:
+#         click.echo(f"There was an error parsing {txt_file}")
+#         click.echo("Check the logs for more details.")
+#         click.echo(error)
--- src/aa_pbs_exporter/pbs_2022_01/parse_pbs_txt_file.py
+++ src/aa_pbs_exporter/pbs_2022_01/parse_pbs_txt_file.py
@@ -1,57 +1,57 @@
-from pathlib import Path
+# from pathlib import Path
 
-from aa_pbs_exporter.pbs_2022_01.helpers.compact_pbs_file import compact_pbs_object
-from aa_pbs_exporter.pbs_2022_01.helpers.expand_pbs_file import expand_pbs_object
-from aa_pbs_exporter.pbs_2022_01.parser.parse_manager import file_parse_manager
-from aa_pbs_exporter.pbs_2022_01.helpers.parse_pbs_file import parse_pbs_file
-from aa_pbs_exporter.pbs_2022_01.models import compact, expanded, collated
+# from aa_pbs_exporter.pbs_2022_01.helpers.compact_pbs_file import compact_pbs_object
+# from aa_pbs_exporter.pbs_2022_01.helpers.expand_pbs_file import expand_pbs_object
+# from aa_pbs_exporter.pbs_2022_01.parser.parse_manager import file_parse_manager
+# from aa_pbs_exporter.pbs_2022_01.helpers.parse_pbs_file import parse_pbs_file
+# from aa_pbs_exporter.pbs_2022_01.models import compact, expanded, collated
 
 
-def parse_pbs_txt_file(
-    txt_file: Path,
-    output_dir: Path | None,
-    debug_dir: Path | None,
-    compact_out: bool = True,
-    expanded_out: bool = True,
-) -> tuple[raw.BidPackage, compact.BidPackage | None, expanded.BidPackage | None]:
-    """A convenience function to go all the way from txt to compact to expanded with optional save to file."""
+# def parse_pbs_txt_file(
+#     txt_file: Path,
+#     output_dir: Path | None,
+#     debug_dir: Path | None,
+#     compact_out: bool = True,
+#     expanded_out: bool = True,
+# ) -> tuple[raw.BidPackage, compact.BidPackage | None, expanded.BidPackage | None]:
+#     """A convenience function to go all the way from txt to compact to expanded with optional save to file."""
 
-    compact_bid_package: None | compact.BidPackage = None
-    expanded_bid_package: None | expanded.BidPackage = None
-    manager = file_parse_manager(
-        source_path=txt_file,
-        debug_dir=debug_dir,
-    )
-    raw_bid_package = parse_pbs_file(
-        file_in=txt_file,
-        save_dir=output_dir,
-        manager=manager,
-        file_name=None,
-        overwrite=False,
-    )
-    if compact_out:
-        compact_debug_path = compact.BidPackage.default_debug_file(
-            debug_dir=debug_dir, name=raw_bid_package.default_file_name()
-        )
-        compact_bid_package = compact_pbs_object(
-            save_dir=output_dir,
-            file_name=None,
-            overwrite=False,
-            debug_file=compact_debug_path,
-            raw_bid_package=raw_bid_package,
-        )
-        if expanded_out:
-            assert compact_bid_package is not None
-            expanded_debug_path = expanded.BidPackage.default_debug_file(
-                debug_dir=debug_dir,
-                name=compact_bid_package.default_file_name(),
-            )
-            expanded_bid_package = expand_pbs_object(
-                save_dir=output_dir,
-                file_name=None,
-                overwrite=False,
-                debug_file=expanded_debug_path,
-                compact_bid_package=compact_bid_package,
-            )
+#     compact_bid_package: None | compact.BidPackage = None
+#     expanded_bid_package: None | expanded.BidPackage = None
+#     manager = file_parse_manager(
+#         source_path=txt_file,
+#         debug_dir=debug_dir,
+#     )
+#     raw_bid_package = parse_pbs_file(
+#         file_in=txt_file,
+#         save_dir=output_dir,
+#         manager=manager,
+#         file_name=None,
+#         overwrite=False,
+#     )
+#     if compact_out:
+#         compact_debug_path = compact.BidPackage.default_debug_file(
+#             debug_dir=debug_dir, name=raw_bid_package.default_file_name()
+#         )
+#         compact_bid_package = compact_pbs_object(
+#             save_dir=output_dir,
+#             file_name=None,
+#             overwrite=False,
+#             debug_file=compact_debug_path,
+#             raw_bid_package=raw_bid_package,
+#         )
+#         if expanded_out:
+#             assert compact_bid_package is not None
+#             expanded_debug_path = expanded.BidPackage.default_debug_file(
+#                 debug_dir=debug_dir,
+#                 name=compact_bid_package.default_file_name(),
+#             )
+#             expanded_bid_package = expand_pbs_object(
+#                 save_dir=output_dir,
+#                 file_name=None,
+#                 overwrite=False,
+#                 debug_file=expanded_debug_path,
+#                 compact_bid_package=compact_bid_package,
+#             )
 
-    return (raw_bid_package, compact_bid_package, expanded_bid_package)
+#     return (raw_bid_package, compact_bid_package, expanded_bid_package)
